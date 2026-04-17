ecf1bb299536fce6549ce9fa83e206480a090f87
chariitaoufik97 <charii.taofik97@gmail.com>
2026-04-17 14:57:28 +0200

    [BE]feat: Creazione progetto

---
+++ D4S.Project.GaraAuto/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace D4S.Project.GaraAuto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
